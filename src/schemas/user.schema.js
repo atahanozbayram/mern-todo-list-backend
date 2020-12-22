0b143bc02b65b54fc4b9819f76a40c21dc267ea3
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-12-22 12:06:23 +0300

    change schema to better style

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const alternativeValidator = require('../plugins/unique.plugin');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
 	_id: mongoose.Types.ObjectId,
-	name: {
-		first: { type: String, required: true },
-		last: { type: String, required: true },
-	},
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	passwordHash: { type: String, required: true },
 });
 
-UserSchema.plugin(uniqueValidator);
+// UserSchema.plugin(uniqueValidator);
+UserSchema.plugin(alternativeValidator);
 
 module.exports = UserSchema;
