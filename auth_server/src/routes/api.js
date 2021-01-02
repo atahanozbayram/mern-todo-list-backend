006d5526f1bf23a57834d07bb9a7a0d9aca10780
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-02 17:46:11 +0300

    remove authServer subfolder completely

--- auth_server/src/routes/api.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const refreshRoute = require('./refresh_token/refresh-token');
-
-route.use('/refreshToken', refreshRoute);
-module.exports = route;
