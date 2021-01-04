407c2a326488f062980765d86c01e4cfbcce89b2
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-04 17:34:54 +0300

    Add boilerplate code for todo route

---
+++ src/routes/todo.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { check, validationResult } = require('express-validator');
+const connection = require('@root/db-connection');
+
+async function main() {
+	const todoRoute = express.Router();
+}
+
+main();
