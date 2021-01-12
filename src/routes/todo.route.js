643fda98792319ecb92301903a323cd39768962f
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-12 21:03:26 +0300

    change reduce the amount of code for matching user and todos

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -6,6 +6,64 @@ const UserSchema = require('@root/src/schemas/user.schema');
 const TodoSchema = require('@root/src/schemas/todo.schema');
 
 async function main() {
+	function todoUserMatch(req, res, next) {
+		const errors = validationResult(req);
+		if (errors.isEmpty() == false) {
+			res.status(400).json({ errors: errors.array() });
+			return;
+		}
+
+		const UserModel = connection.model('User', UserSchema);
+		const { email: userEmail } = req.authorization.user;
+
+		UserModel.findOne({ email: userEmail }, function (err, userDoc) {
+			if (err) {
+				console.error(err);
+				res
+					.status(500)
+					.json({ errors: [{ msg: 'Some error occured on server side.' }] });
+				return;
+			}
+
+			if (userDoc == null) {
+				res
+					.status(400)
+					.json({ errors: [{ msg: "User doesn't exist in database." }] });
+				return;
+			}
+
+			const TodoModel = connection.model('Todo', TodoSchema);
+			const { id } = req.body;
+
+			TodoModel.findOne({ _id: id }, function (err, todoDoc) {
+				if (err) {
+					console.error('error: %o', err);
+					res.status(500).json({ errors: [err] });
+					return;
+				}
+
+				if (todoDoc == null) {
+					console.error('todoDoc == null: %o', todoDoc);
+					res.status(500).json({
+						errors: [{ msg: 'Given todo does not exists in database.' }],
+					});
+					return;
+				}
+
+				// check if the user and todos user id same
+				if (userDoc._id.equals(todoDoc.user) == false) {
+					res.status(400).json({
+						errors: [{ msg: 'Unauthorized to delete given todo.' }],
+					});
+					return;
+				}
+
+				// proceed to the next function
+				next();
+			});
+		});
+	}
+
 	const todoRoute = express.Router();
 
 	todoRoute.post(
@@ -90,6 +148,7 @@ async function main() {
 				.isMongoId()
 				.bail()
 				.withMessage('id field must be mongo id.'),
+			todoUserMatch,
 		],
 		function (req, res, next) {
 			const errors = validationResult(req);
@@ -99,55 +158,26 @@ async function main() {
 				return;
 			}
 
-			// check for todo's user and the requesting user.
-			// if they are not same don't delete it.
-			const { email: userEmail } = req.authorization.user;
-			const UserModel = connection.model('User', UserSchema);
-
-			UserModel.findOne({ email: userEmail }, function (err, userDoc) {
+			const TodoModel = connection.model('Todo', TodoSchema);
+			const { id } = req.body;
+			TodoModel.findOneAndDelete({ _id: id }, function (err, todoDoc) {
 				if (err) {
-					console.error(err);
-					res
-						.status(500)
-						.json({ errors: [{ msg: 'Some error occured on server side.' }] });
+					console.error('error: %o', err);
+					res.status(500).json({ errors: [err] });
 					return;
 				}
 
-				if (userDoc == null) {
-					res
-						.status(400)
-						.json({ errors: [{ msg: "User doesn't exist in database." }] });
+				if (todoDoc == null) {
+					console.error('todoDoc == null: %o', todoDoc);
+					res.status(500).json({
+						errors: [{ msg: 'given todo does not exist in database.' }],
+					});
 					return;
 				}
 
-				const TodoModel = connection.model('Todo', TodoSchema);
-				const { id } = req.body;
-				TodoModel.findOneAndDelete({ _id: id }, function (err, todoDoc) {
-					if (err) {
-						console.error('error: %o', err);
-						res.status(500).json({ errors: [err] });
-						return;
-					}
-
-					if (todoDoc == null) {
-						console.error('todoDoc == null: %o', todoDoc);
-						res.status(500).json({
-							errors: [{ msg: 'given todo does not exist in database.' }],
-						});
-						return;
-					}
-					// check if the user and todos user id same
-					if (userDoc._id.equals(todoDoc.user) == false) {
-						res
-							.status(400)
-							.json({ errors: [{ msg: 'Unauthorized to delete given todo' }] });
-						return;
-					}
-
-					res
-						.status(200)
-						.json({ msg: 'todo deleted successfully.', todo: todoDoc });
-				});
+				res
+					.status(200)
+					.json({ msg: 'todo deleted successfully.', todo: todoDoc });
 			});
 		}
 	);
@@ -161,11 +191,7 @@ async function main() {
 				.isMongoId()
 				.bail()
 				.withMessage('id field must be mongo id.'),
-			check('text').isString().bail().withMessage('text field must be text'),
-			check('completed')
-				.isBoolean()
-				.bail()
-				.withMessage('completed field must be boolean.'),
+			todoUserMatch,
 		],
 		function (req, res, next) {
 			const errors = validationResult(req);
@@ -174,6 +200,28 @@ async function main() {
 				res.status(400).json({ errors: errors.array() });
 				return;
 			}
+
+			const TodoModel = connection.model('Todo', TodoSchema);
+			const { id } = req.body;
+			TodoModel.findOne({ _id: id }, function (err, todoDoc) {
+				if (err) {
+					console.err(err);
+					res
+						.status(500)
+						.json({ errors: [{ msg: 'Some error occured on server side.' }] });
+					return;
+				}
+
+				if (todoDoc == null) {
+					res.status(400).json({ errors: [{ msg: 'Todo is non existent.' }] });
+					return;
+				}
+
+				todoDoc.completed = !todoDoc.completed;
+				todoDoc.save();
+
+				res.status(200).json({ msg: 'completed field toggled', todo: todoDoc });
+			});
 		}
 	);
 
