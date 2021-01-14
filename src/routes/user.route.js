ff1a6f94474007f6f471073d035fe2c7df0214d0
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-14 16:28:52 +0300

    Refactor to tidy up code and make it functional

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,3 +1,4 @@
+'use strict';
 const axios = require('axios').default;
 const express = require('express');
 const bcrypt = require('bcrypt');
@@ -6,217 +7,214 @@ const { check, validationResult } = require('express-validator');
 const UserSchema = require('@root/src/schemas/user.schema');
 const connection = require('@root/db-connection');
 
-async function main() {
+const routes = function () {
 	const userRoute = express.Router();
+	const validations = {};
+	const exp = {};
 
-	userRoute.post(
-		'/register',
-		[
-			check('firstName')
-				.exists()
-				.bail()
-				.withMessage('firstName field must be not empty')
-				.isString()
-				.bail()
-				.withMessage('firstName field must be string')
-				.isLength({ max: 40 })
-				.bail()
-				.withMessage('firstName field cannot be longer than 40 characters'),
-			check('lastName')
-				.exists()
-				.bail()
-				.withMessage('lastName field cannot be empty')
-				.isString()
-				.bail()
-				.withMessage('lastName field must be string')
-				.isLength({ max: 40 })
-				.bail()
-				.withMessage('lastName field cannot be longer than 40 characters'),
-			check('email')
-				.exists()
-				.bail()
-				.withMessage('email field must be not empty')
-				.isEmail()
-				.bail()
-				.withMessage('email field must be valid email address')
-				.isLength({ max: 254 })
-				.bail()
-				.withMessage('email field cannot be longer than 254 characters'),
-			check('password')
-				.exists()
-				.bail()
-				.withMessage('password field must be not empty')
-				.bail()
-				.isString()
-				.withMessage('password field must be string')
-				.isLength({ min: 8 })
-				.bail()
-				.withMessage('password field must be at least 8 characters'),
-		],
-		function (req, res, next) {
-			const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				res.status(400).json({ errors: errors.array() });
-				return;
-			}
-
-			const { firstName, lastName, email, password } = req.body;
-
-			const passwordSalt = bcrypt.genSaltSync(1);
-			const passwordHash = bcrypt.hashSync(password, passwordSalt);
-
-			const UserModel = connection.model('User', UserSchema);
-			const user = new UserModel({
-				_id: new mongoose.Types.ObjectId(),
-				firstName,
-				lastName,
-				email,
-				passwordHash,
-			});
-
-			user
-				.save()
-				.then((result) => {
-					res.status(200).json({
-						msg: 'User registered successfully',
-						firstName: firstName,
-						lastName: lastName,
-						email: email,
-					});
-				})
-				.catch((err) => {
-					res.status(400).send(err);
-				});
+	exp.register = function (req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ errors: errors.array() });
+			return;
 		}
-	);
 
-	userRoute.post(
-		'/login',
-		[
-			check('email')
-				.exists()
-				.bail()
-				.notEmpty()
-				.bail()
-				.withMessage('email field must not be empty')
-				.isEmail()
-				.bail()
-				.withMessage('email field must be valid email'),
-			check('password')
-				.exists()
-				.bail()
-				.notEmpty()
-				.bail()
-				.withMessage('password field must not be empty')
-				.isString()
-				.bail()
-				.withMessage('password field must be string'),
-		],
-		function (req, res, next) {
-			const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				res.status(400).json({ errors: errors.array() });
-				return;
-			}
+		const { firstName, lastName, email, password } = req.body;
 
-			const { email, password } = req.body;
+		const passwordSalt = bcrypt.genSaltSync(1);
+		const passwordHash = bcrypt.hashSync(password, passwordSalt);
 
-			// check if the email and password match
-			const UserModel = connection.model('User', UserSchema);
-			UserModel.findOne({ email: email }, async function (err, user) {
-				if (err) {
-					console.error(err);
-					res.status(500).json({
-						errors: [{ msg: 'Unknown error occured in the server.' }],
-					});
-					return; // terminate the function
-				}
+		const UserModel = connection.model('User', UserSchema);
+		const user = new UserModel({
+			_id: new mongoose.Types.ObjectId(),
+			firstName,
+			lastName,
+			email,
+			passwordHash,
+		});
 
-				if (user === null) {
-					res.status(400).json({ errors: [{ msg: 'Non existent email.' }] });
-					return;
-				}
-
-				// if program reaches here, that means we have a valid user. Now check for password validity.
-				// this if check if it fails
-				if (bcrypt.compareSync(password, user.passwordHash) === false) {
-					res.status(400).json({ errors: [{ msg: 'Invalid password.' }] });
-					return;
-				}
-
-				// send request to obtain refresh token
-				const axiosRes = await axios({
-					method: 'POST',
-					baseURL: process.env.AUTH_SERVER_IP || 'http://localhost:5000/api',
-					url: 'refreshToken/request',
-					data: {
-						email: email,
-						password: password,
-					},
-				}).catch((axiosErr) => {
-					console.error(axiosErr);
-					res
-						.status(500)
-						.json({ errors: [{ msg: 'some error occured in server side.' }] });
-					return;
+		user
+			.save()
+			.then((result) => {
+				res.status(200).json({
+					msg: 'User registered successfully',
+					firstName: firstName,
+					lastName: lastName,
+					email: email,
 				});
-
-				if (axiosRes === undefined) return;
-
-				res
-					.status(200)
-					.cookie('refreshToken', axiosRes.data.refreshToken)
-					.send();
+			})
+			.catch((err) => {
+				res.status(400).send(err);
 			});
+	};
+
+	exp.login = function (req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ errors: errors.array() });
+			return;
 		}
-	);
 
-	userRoute.post(
-		'/logout',
-		[
-			check('logoutAll')
-				.exists()
-				.bail()
-				.withMessage('logoutAll field must exist.')
-				.isBoolean()
-				.bail()
-				.withMessage('logoutAll field must be boolean.'),
-		],
-		async function (req, res, next) {
-			const errors = validationResult(req);
-			if (errors.isEmpty() === false) {
-				res.status(400).json({ errors: errors.array() });
+		const { email, password } = req.body;
+
+		// check if the email and password match
+		const UserModel = connection.model('User', UserSchema);
+		UserModel.findOne({ email: email }, async function (err, user) {
+			if (err) {
+				console.error(err);
+				res.status(500).json({
+					errors: [{ msg: 'Unknown error occured in the server.' }],
+				});
+				return; // terminate the function
+			}
+
+			if (user === null) {
+				res.status(400).json({ errors: [{ msg: 'Non existent email.' }] });
 				return;
 			}
 
-			// check for cookie existense
-			if (req.cookies['refreshToken'] === undefined) {
-				res
-					.status(400)
-					.json({ errors: [{ msg: 'refreshToken cookie is missing.' }] });
+			// if program reaches here, that means we have a valid user. Now check for password validity.
+			// this if check if it fails
+			if (bcrypt.compareSync(password, user.passwordHash) === false) {
+				res.status(400).json({ errors: [{ msg: 'Invalid password.' }] });
 				return;
 			}
 
+			// send request to obtain refresh token
 			const axiosRes = await axios({
-				method: 'DELETE',
+				method: 'POST',
 				baseURL: process.env.AUTH_SERVER_IP || 'http://localhost:5000/api',
-				url: '/refreshToken/delete',
+				url: 'refreshToken/request',
 				data: {
-					token: req.cookies.refreshToken,
-					logoutAll: req.body.logoutAll,
+					email: email,
+					password: password,
 				},
 			}).catch((axiosErr) => {
 				console.error(axiosErr);
-				res.status(500).json(axiosErr.response.data);
+				res
+					.status(500)
+					.json({ errors: [{ msg: 'some error occured in server side.' }] });
 				return;
 			});
 
 			if (axiosRes === undefined) return;
 
-			res.status(200).clearCookie('refreshToken').json(axiosRes.data);
-		}
-	);
-	module.exports = userRoute;
-}
+			res.status(200).cookie('refreshToken', axiosRes.data.refreshToken).send();
+		});
+	};
 
-main();
+	exp.logout = async function (req, res, next) {
+		const errors = validationResult(req);
+		if (errors.isEmpty() === false) {
+			res.status(400).json({ errors: errors.array() });
+			return;
+		}
+
+		// check for cookie existense
+		if (req.cookies['refreshToken'] === undefined) {
+			res
+				.status(400)
+				.json({ errors: [{ msg: 'refreshToken cookie is missing.' }] });
+			return;
+		}
+
+		const axiosRes = await axios({
+			method: 'DELETE',
+			baseURL: process.env.AUTH_SERVER_IP || 'http://localhost:5000/api',
+			url: '/refreshToken/delete',
+			data: {
+				token: req.cookies.refreshToken,
+				logoutAll: req.body.logoutAll,
+			},
+		}).catch((axiosErr) => {
+			console.error(axiosErr);
+			res.status(500).json(axiosErr.response.data);
+			return;
+		});
+
+		if (axiosRes === undefined) return;
+
+		res.status(200).clearCookie('refreshToken').json(axiosRes.data);
+	};
+
+	validations.register = [
+		check('firstName')
+			.exists()
+			.bail()
+			.withMessage('firstName field must be not empty')
+			.isString()
+			.bail()
+			.withMessage('firstName field must be string')
+			.isLength({ max: 39 })
+			.bail()
+			.withMessage('firstName field cannot be longer than 39 characters'),
+		check('lastName')
+			.exists()
+			.bail()
+			.withMessage('lastName field cannot be empty')
+			.isString()
+			.bail()
+			.withMessage('lastName field must be string')
+			.isLength({ max: 39 })
+			.bail()
+			.withMessage('lastName field cannot be longer than 39 characters'),
+		check('email')
+			.exists()
+			.bail()
+			.withMessage('email field must be not empty')
+			.isEmail()
+			.bail()
+			.withMessage('email field must be valid email address')
+			.isLength({ max: 253 })
+			.bail()
+			.withMessage('email field cannot be longer than 253 characters'),
+		check('password')
+			.exists()
+			.bail()
+			.withMessage('password field must be not empty')
+			.bail()
+			.isString()
+			.withMessage('password field must be string')
+			.isLength({ min: 7 })
+			.bail()
+			.withMessage('password field must be at least 7 characters'),
+	];
+
+	validations.login = [
+		check('email')
+			.exists()
+			.bail()
+			.notEmpty()
+			.bail()
+			.withMessage('email field must not be empty')
+			.isEmail()
+			.bail()
+			.withMessage('email field must be valid email'),
+		check('password')
+			.exists()
+			.bail()
+			.notEmpty()
+			.bail()
+			.withMessage('password field must not be empty')
+			.isString()
+			.bail()
+			.withMessage('password field must be string'),
+	];
+
+	validations.logout = [
+		check('logoutAll')
+			.exists()
+			.bail()
+			.withMessage('logoutAll field must exist.')
+			.isBoolean()
+			.bail()
+			.withMessage('logoutAll field must be boolean.'),
+	];
+	userRoute.post('/register', validations.register, exp.register);
+	userRoute.post('/login', validations.login, exp.login);
+	userRoute.post('/logout', validations.logout, exp.logout);
+
+	return userRoute;
+};
+
+module.exports = routes;
