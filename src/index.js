7720bbb9a8a89d145effc0ffae98c9a5266f6119
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-23 09:32:06 +0300

    Add cors middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ require('module-alias/register'); // this line should be on first line whatever 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsValidation = require('./express_middlewares/cors-validation');
 const apiRoute = require('@root/src/routes/api.route');
 
 async function main() {
 	const app = express();
 
+	app.use(cors({ credentials: true, origin: corsValidation }));
 	app.use(
 		bodyParser.urlencoded({
 			extended: true,
