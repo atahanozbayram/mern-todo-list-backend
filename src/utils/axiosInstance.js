2e529e203f890eb49b18dfae512fc3cfc87a5d92
Atahan Ozbayram <atahan.ozbayram@hotmail.com>
2021-03-01 19:54:36 +0300

    Add axiosInstance to use on multiple locations

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,9 @@
+const axios = require('axios').default;
+
+const auth_server_host = process.env.AUTH_SERVER_HOST || 'localhost:5000';
+
+const axiosAuthInstance = axios.create({
+	baseURL: `${auth_server_host}/api`,
+});
+
+export { axiosAuthInstance };
