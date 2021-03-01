33a4bf4fb568e1c2168c978d003243b29c727a76
Atahan Ozbayram <atahan.ozbayram@hotmail.com>
2021-03-01 19:54:56 +0300

    Add and use axiosInstance

--- src/express_middlewares/authorization.middleware.js
+++ src/express_middlewares/authorization.middleware.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const axios = require('axios').default;
+const { axiosAuthInstance } = require('@root/src/utils/axiosInstance');
 const jwt = require('jsonwebtoken');
 
 const isAuthorized = async function (req, res, next) {
@@ -32,48 +33,50 @@ const isAuthorized = async function (req, res, next) {
 		// check for validity of refreshToken
 		const auth_server_host =
 			process.env.AUTH_SERVER_HOST || 'http://localhost:5000/api';
-		const axiosRes = await axios({
+
+		axiosAuthInstance({
 			method: 'GET',
-			baseURL: `${auth_server_host}/api`,
 			url: 'refreshToken/validate',
 			data: {
 				token: refreshToken,
 			},
-		}).catch((axiosErr) => {
-			console.error('axiosErr: %o', axiosErr.response.data);
-			res.status(401).json({ errors: [{ msg: 'Unauthorized access' }] });
-		});
+		})
+			.then((axiosRes) => {
+				const decodedRefreshToken = jwt.decode(refreshToken);
+				res.cookie(
+					'accessToken',
+					jwt.sign(
+						{
+							email: decodedRefreshToken.email,
+							user_id: decodedRefreshToken.user_id,
+							firstName: decodedRefreshToken.firstName,
+							lastName: decodedRefreshToken.lastName,
+							date: Date.now(),
+						},
+						process.env.ACCESS_TOKEN_SECRET,
+						{ algorithm: 'HS256' }
+					),
+					{
+						sameSite: 'none',
+						httpOnly: false,
+						secure: true,
+					}
+				);
 
-		if (axiosRes === undefined) return;
+				req.authorization = {
+					user: {
+						email: decodedRefreshToken.email,
+						id: decodedRefreshToken.user_id,
+					},
+				};
+				next();
+				return;
+			})
+			.catch((axiosErr) => {
+				console.error('axiosErr: %o', axiosErr.response.data);
+				res.status(401).json({ errors: [{ msg: 'Unauthorized access' }] });
+			});
 
-		const decodedRefreshToken = jwt.decode(refreshToken);
-		res.cookie(
-			'accessToken',
-			jwt.sign(
-				{
-					email: decodedRefreshToken.email,
-					user_id: decodedRefreshToken.user_id,
-					firstName: decodedRefreshToken.firstName,
-					lastName: decodedRefreshToken.lastName,
-					date: Date.now(),
-				},
-				process.env.ACCESS_TOKEN_SECRET,
-				{ algorithm: 'HS256' }
-			),
-			{
-				sameSite: 'none',
-				httpOnly: false,
-				secure: true,
-			}
-		);
-
-		req.authorization = {
-			user: {
-				email: decodedRefreshToken.email,
-				id: decodedRefreshToken.user_id,
-			},
-		};
-		next();
 		return;
 	}
 
